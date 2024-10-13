e2a6ff69964931287ba15d9a30112cdc50d70c21
mateoPuntoJar <manuelmtfz@gmail.com>
2024-10-13 18:04:29 +0200

    Added functionalities

---
+++ script.js
@@ -0,0 +1,28 @@
+/* Botón para ir a Reserva */
+function goToReserve(){
+    window.open('reserva.html', '_blank');
+}
+
+document.getElementById('reserveButton').addEventListener('click', goToReserve);
+
+/* Botón para ir a Contacto */
+function goToContacte(){
+    window.open('contacto.html', '_blank');
+}
+
+document.getElementById('contacteButton').addEventListener('click', goToContacte);
+
+/* Botón para ir a Información sobre Reservas */
+function goToInformation(){
+    window.open('información-de-reservas.html', '_blank');
+}
+
+document.getElementById('reserve-information-button').addEventListener('click', goToInformation);
+
+
+/* Botón para ir a Información sobre Reservas */
+function goToInformation(){
+    window.open('información-de-reservas.html', '_blank');
+}
+
+document.getElementById('information-reserve-button').addEventListener('click', goToInformation);
