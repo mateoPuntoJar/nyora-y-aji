a0c7e64a9135f737f00b39dfbae4e791cd8e6d3d
mateoPuntoJar <manuelmtfz@gmail.com>
2025-03-18 19:11:14 +0100

    Formatted Files

--- php/index.php
+++ php/index.php
@@ -1,25 +1,36 @@
 <?php
 // http://localhost/nyora-y-aji/php/index.php
-?>
-        <?php
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $nombre = $_POST['nombre'];  # Dentro de los corchetes hay que indicar el valor del atributo name de los inputs del formulario
-                $email = $_POST['email'];
-                $phone = $_POST['phone'];
-                $message = $_POST['message'];
-                $check = isset($_POST['check']) ? "Aceptado" : "No aceptado";
 
-                $mensaje = "Este mensaje fue enviado por " . $nombre . "<br>";
-                $mensaje .= "Su e-mail es: " . $email . "<br>";
-                $mensaje .= "Su teléfono es: " . $phone ."<br>";
-                $mensaje .= "Mensaje: " . $message ."<br>";
-                $mensaje .= $check ."<br>";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST['nombre'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $phone = $_POST['phone'] ?? '';
+    $message = $_POST['message'] ?? '';
+    $check = isset($_POST['check']) ? "Aceptado" : "No aceptado";
 
-                $destinatario = "manuelprra@gmail.com";
-                $asunto = "Mail enviado desde Contacto Eventos";
+    // Construir el cuerpo del correo
+    $mensaje = "Este mensaje fue enviado por: " . htmlspecialchars($nombre) . "\n";
+    $mensaje .= "Su e-mail es: " . htmlspecialchars($email) . "\n";
+    $mensaje .= "Su teléfono es: " . htmlspecialchars($phone) . "\n";
+    $mensaje .= "Mensaje: " . htmlspecialchars($message) . "\n";
+    $mensaje .= "Acepta términos: " . $check . "\n";
 
-                mail($destinatario, $asunto, $mensaje, $header);
+    // Encabezados para el correo
+    $header = "From: $email\r\n";
+    $header .= "Reply-To: $email\r\n";
+    $header .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-                header("Location:http://localhost/nyora-y-aji/exito.html");
-            }
+    // Destinatario y asunto
+    $destinatario = "manuelprra@gmail.com";
+    $asunto = "Mail enviado desde Contacto Eventos";
+
+    // Enviar el correo
+    if (mail($destinatario, $asunto, $mensaje, $header)) {
+        // Redirigir si el correo fue enviado correctamente
+        header("Location: http://localhost/nyora-y-aji/exito.html");
+        exit();
+    } else {
+        echo "Error al enviar el correo.";
+    }
+}
 ?>
